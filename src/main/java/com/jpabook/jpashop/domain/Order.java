c58da0128ee5f1f550ab7b31fbaa9635a9fa88b4
mitsui3point <rladbsgh0331@gmail.com>
2022-11-17 16:29:57 +0900

    feat($domain): update 엔티티 설계시 주의점 update code - @XToOne fetch=LAZY - Order cascade=ALL - Category and Order 연관관계 methods
    update 내용
    - 엔티티에는 가급적 Setter를 사용하지 말자
    - 모든 연관관계는 지연로딩으로 설정!
    - 컬렉션은 필드에서 초기화 하자.
    
    - cascade persist() 전파 추가

--- src/main/java/com/jpabook/jpashop/domain/Order.java
+++ src/main/java/com/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.CascadeType.*;
+import static javax.persistence.FetchType.LAZY;
+
 @Entity(name = "orders")
 @Getter
 @Setter
@@ -17,14 +20,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -32,4 +35,20 @@ public class Order {
 
     @Enumerated(value = EnumType.STRING)
     private OrderStatus orderStatus;//주문상태[ORDER, CANCEL]
+
+    //==연관관계 메서드==
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
