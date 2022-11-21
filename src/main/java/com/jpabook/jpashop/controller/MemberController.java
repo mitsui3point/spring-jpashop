e82335a0509ac315bf68afa9229931716cdfe37d
mitsui3point <rladbsgh0331@gmail.com>
2022-11-21 15:22:14 +0900

    feat($controller, $resources/template): add - add find members form

--- src/main/java/com/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/com/jpabook/jpashop/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.jpabook.jpashop.domain.Address;
 import com.jpabook.jpashop.domain.Member;
 import com.jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -40,5 +44,19 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String members(Model model) {
+        List<MemberDTO> memberDTOs = memberService.findAll().stream()
+                .map(member -> {
+                    return MemberDTO.builder()
+                            .id(member.getId())
+                            .name(member.getName())
+                            .address(member.getAddress())
+                            .build();
+                }).collect(Collectors.toList());
+        model.addAttribute("members", memberDTOs);
+        return "member/memberList";
     }
 }
---
+++ src/main/java/com/jpabook/jpashop/controller/MemberDTO.java
@@ -0,0 +1,15 @@
+package com.jpabook.jpashop.controller;
+
+import com.jpabook.jpashop.domain.Address;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class MemberDTO {
+    private Long id;
+    private String name;
+    private Address address;
+}
