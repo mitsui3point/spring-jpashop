ad6568de5306574a4e3627343bc0e73c24672fc0
mitsui3point <rladbsgh0331@gmail.com>
2022-11-21 22:14:47 +0900

    feat($controller, $resources.templates): update - update items(merge 방식)

--- src/main/java/com/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/com/jpabook/jpashop/controller/ItemController.java
@@ -1,12 +1,15 @@
 package com.jpabook.jpashop.controller;
 
 import com.jpabook.jpashop.domain.item.Book;
+import com.jpabook.jpashop.domain.item.Item;
 import com.jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -45,5 +48,39 @@ public class ItemController {
     public String itemList(Model model) {
         model.addAttribute("items", itemService.findItems());
         return "items/itemList";
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable(value = "itemId") Long itemId, Model model) {
+        Book item = (Book) itemService.findOne(itemId);
+
+        BookForm bookForm = new BookForm();
+        bookForm.setId(item.getId());
+        bookForm.setName(item.getName());
+        bookForm.setPrice(item.getPrice());
+        bookForm.setStockQuantity(item.getStockQuantity());
+        bookForm.setIsbn(item.getIsbn());
+        bookForm.setAuthor(item.getAuthor());
+
+        model.addAttribute("bookForm", bookForm);
+
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String updateItem(@PathVariable(value = "itemId") Long itemId,
+                             @ModelAttribute("bookForm") @Valid BookForm bookForm) {
+        Book item = new Book();
+
+        item.setId(bookForm.getId()); //id 세팅을 임의로하는것은 권한없는 유저가 수정을하거나, id를 임의로 조작변경이 가능하기 때문에 권장하지않는 방법이다..
+        item.setName(bookForm.getName());
+        item.setPrice(bookForm.getPrice());
+        item.setStockQuantity(bookForm.getStockQuantity());
+        item.setAuthor(bookForm.getAuthor());
+        item.setIsbn(bookForm.getIsbn());
+
+        itemService.saveItem(item);
+
+        return "redirect:/items";
     }
 }
