71f5901c13d3db557c7abbc3a8238a7a5a324d6a
mitsui3point <rladbsgh0331@gmail.com>
2022-12-13 01:35:01 +0900

    feat($api, $root, $domain, $repository, $service): update - update 주문 + 배송정보 + 회원을 조회하는 API
    - 지연 로딩 때문에 발생하는 성능 문제를 단계적으로 해결해보자.
    
    - 간단한 주문 조회 V2: 엔티티를 DTO로 변환
    : 엔티티를 DTO로 변환하는 일반적인 방법이다.
    : 쿼리가 총 1 + N + N번 실행된다. (v1과 쿼리수 결과는 같다.)
    : order 조회 1번(order 조회 결과 수가 N이 된다.)
    : order -> member 지연 로딩 조회 N 번
    : order -> delivery 지연 로딩 조회 N 번
    
    예) order의 결과가 4개면 최악의 경우 1 + 4 + 4번 실행된다.(최악의 경우) 지연로딩은 영속성 컨텍스트에서 조회하므로, 이미 조회된 경우 쿼리를 생략한다.

--- src/main/java/com/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/com/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,15 +1,23 @@
 package com.jpabook.jpashop.api;
 
+import com.jpabook.jpashop.domain.Address;
 import com.jpabook.jpashop.domain.Category;
 import com.jpabook.jpashop.domain.Order;
 import com.jpabook.jpashop.domain.OrderItem;
+import com.jpabook.jpashop.domain.enums.OrderStatus;
 import com.jpabook.jpashop.repository.OrderSearch;
 import com.jpabook.jpashop.service.OrderService;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 /**
  * xToOne(ManyToOne, OneToOne)
@@ -37,5 +45,36 @@ public class OrderSimpleApiController {
             order.getDelivery().getDeliveryStatus();//lazy 강제 초기화
         }
         return orders;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        //ORDER 2개
+        //N + 1 -> 1 + 회원 N + 배송 N
+        return orderService.findAll(new OrderSearch())
+                .stream()
+                .map(order -> SimpleOrderDto.builder()
+                            .order(order)
+                            .build()
+                )
+                .collect(Collectors.toList());
+    }
+
+    @Getter
+    @NoArgsConstructor
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        @Builder
+        public SimpleOrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();//lazy 초기화
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();//lazy 초기화
+        }
     }
 }
--- src/test/java/com/jpabook/jpashop/api/OrderSimpleApiControllerTest.java
+++ src/test/java/com/jpabook/jpashop/api/OrderSimpleApiControllerTest.java
@@ -15,16 +15,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+import static com.jpabook.jpashop.api.OrderSimpleApiController.SimpleOrderDto;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional(readOnly = true)
 class OrderSimpleApiControllerTest {
     private static final String BASE_URL = "/api";
-    private static final String ORDER_GET_URL = BASE_URL + "/v1/simple-orders";
+    private static final String ORDER_GET_V1_URL = BASE_URL + "/v1/simple-orders";
+    private static final String ORDER_GET_V2_URL = BASE_URL + "/v2/simple-orders";
+
     @Autowired
     private ObjectMapper mapper;
     @Autowired
@@ -36,11 +40,54 @@ class OrderSimpleApiControllerTest {
 
     @BeforeEach
     void setUp() {
+    }
+
+    @Test
+    void 주문_전체조회_V1() throws Exception {
+        //given
         initOrders = em.createQuery("select o from orders o join o.member m ",
                         Order.class)
                 .getResultList()
                 .toArray(Order[]::new);
         initOrderObjectGraph();
+        String expected = mapper.writeValueAsString(initOrders);
+
+        //when
+        ResultActions perform = mvc.perform(get(ORDER_GET_V1_URL));
+
+        //then
+        perform.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(expected));
+    }
+
+    @Test
+    void 주문_전체조회_V2() throws Exception {
+        //given
+        System.out.println("==================================given==================================");
+        SimpleOrderDto[] initOrderDto = em.createQuery("select o from orders o join o.member m ", Order.class)
+                .getResultList()
+                .stream()
+                .map(o -> SimpleOrderDto.builder()
+                        .order(o)
+                        .build()
+                )
+                .toArray(SimpleOrderDto[]::new);
+        System.out.println("==================================given==================================");
+
+        String expected = mapper.writeValueAsString(initOrderDto);
+        em.flush();
+        em.clear();
+
+        //when
+        System.out.println("==================================when==================================");
+        ResultActions perform = mvc.perform(get(ORDER_GET_V2_URL));
+        System.out.println("==================================when==================================");
+
+        //then
+        perform.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(expected));
     }
 
     private void initOrderObjectGraph() {
@@ -56,18 +103,4 @@ class OrderSimpleApiControllerTest {
             }
         }
     }
-
-    @Test
-    void 주문_전체조회_V1() throws Exception {
-        //given
-        String expected = mapper.writeValueAsString(initOrders);
-
-        //when
-        ResultActions perform = mvc.perform(get(ORDER_GET_URL));
-
-        //then
-        perform.andDo(print())
-                .andExpect(content().json(expected));
-    }
-
 }
