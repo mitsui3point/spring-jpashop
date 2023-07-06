be76c102a18a5f2243f415bb7112f24a752f3725
mitsui3point <rladbsgh0331@gmail.com>
2023-07-06 23:48:03 +0900

    feat: optimize imports

--- src/main/java/com/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/com/jpabook/jpashop/repository/MemberRepository.java
@@ -4,7 +4,6 @@ import com.jpabook.jpashop.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Map;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     public List<Member> findByName(String name);
